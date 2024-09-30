5f63b483297865ac41ff1c4690e5def3801af1df
Ashawo-bd <ashawobd@gmail.com>
2024-09-30 01:53:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ if (fs.existsSync('config.env')) {
 const API = "https://api-aswin-sparky.koyeb.app";
 const ALIVE = process.env.ALIVE || "I am Alive";
 const HANDLERS = process.env.HANDLER || "^";
-const SESSION_ID = process.env.SESSION_ID || "";
-const SUDO = process.env.SUDO || '917012984396';
+const SESSION_ID = process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:a0ace642e489f4e2583c9a9ef9bcbdff";
+const SUDO = process.env.SUDO || '2347013165595';
 const AUTO_STATUS_VIEW = process.env.AUTO_STATUS_VIEW || "true";
 const ALWAYS_ONLINE = process.env.ALWAYS_ONLINE || "false";
 const DISABLE_PM = process.env.DISABLE_PM  || "false";
